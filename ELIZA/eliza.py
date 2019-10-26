aaee908160db9fb215e3038ae9d73869f5c402e7
Max Engel <maxengel99@gmail.com>
2019-10-26 10:04:24 +0700

    get user input and begin eliza class

---
+++ ELIZA/eliza.py
@@ -0,0 +1,16 @@
+class Eliza:
+
+    def __init__(self):
+        self.temp = 2
+    
+    def hold_conversation():
+        input_string = input("Hi, how are you?\n")
+        while True:
+            print(input_string)
+            input_string = input()
+    
+    def get_response(user_input):
+        print("test")
+
+if __name__ == '__main__':
+    Eliza.hold_conversation()
